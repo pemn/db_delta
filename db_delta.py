d086bd8a86b9d873847ad5b91bc896b6f1a82ebf
Paulo Ernesto <pemn@users.noreply.github.com>
2023-10-18 14:14:27 -0300

    Add files via upload

--- db_delta.py
+++ db_delta.py
@@ -1,17 +1,16 @@
 #!python
 # compare two datasets into a change matrix
-# key_a, key_b: (optional) primary keys (comma separated) to relate records between datasets
-# group_a, group_b: the classification variable to observe changes
-# value_a, value_b: (optional) a numeric variable to as change quantity
+# key_a, key_b: (optional) primary key(s) to relate records between datasets
+# group_a, group_b: the classification variable to observe changes (ex.: lito)
+# value_a, value_b: (optional) a numeric variable with the quantity of change (ex.: volume)
 # condition: (optional) python expression restricting records to be used
 # output_matrix: (optional) path to save the migration table
 # output_records: (optional) path to save the side by side records
-# chart_bar: (optional) display 3d bar charts showing relative mass changes
-# chart_scatter: (optional) display 3d scatter showing the regions that changed
-# v2.0 04/2021 paulo.ernesto
-# v1.0 09/2020 paulo.ernesto
+# v3.0 2023/10 paulo.ernesto
+# v2.0 2021/04 paulo.ernesto
+# v1.0 2020/09 paulo.ernesto
 '''
-usage: $0 input_a*csv,xlsx,bmf,isis key_a:input_a group_a:input_a value_a:input_a input_b*csv,xlsx,bmf,isis key_b:input_b group_b:input_b value_b:input_b condition output_matrix*xlsx output_records*xlsx chart_bar@ chart_scatter@3 x:input_a y:input_a z:input_a
+usage: $0 input_a*csv,xlsx,bmf,isis keys_a#key:input_a groups_a#group_a:input_a value_a:input_a input_b*csv,xlsx,bmf,isis keys_b#key:input_b groups_b#group_b:input_b value_b:input_b condition output_matrix*xlsx percent@ output_records*csv
 '''
 import sys, os.path
 import numpy as np
@@ -21,27 +20,51 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.patches as mpatches
 
 # import modules from a pyz (zip) file with same name as scripts
-sys.path.append(os.path.splitext(sys.argv[0])[0] + '.pyz')
+sys.path.insert(0, os.path.splitext(sys.argv[0])[0] + '.pyz')
 
-from _gui import usage_gui, pd_load_dataframe, pd_save_dataframe
+from _gui import usage_gui, pd_load_dataframe, log
 
 def fn_eye_style(d, s = 'background-color: lightgray'):
   return pd.DataFrame(np.where(np.eye(d.shape[0], d.shape[1]), s, ''), index=d.index, columns=d.columns)
 
+def pd_diff_bar_2d(df, page_size = None):
+  if page_size is not None:
+    page_size = max(1, int(page_size))
+    if len(df) > page_size:
+      bs = np.argsort(np.sum(df, 1))
+      df = df.iloc[bs[-page_size:]]
+  # create a grid on the output image that fits the data
+  nrows = int(np.sqrt(len(df)))
+  ncols = int(np.ceil(len(df) / nrows))
+  fig, ax = plt.subplots(nrows, ncols, tight_layout=True, sharex=True, sharey=True, squeeze=False, figsize=np.multiply(plt.rcParams["figure.figsize"], 2))
+  i = 0
+  for ri,rd in df.iterrows():
+    #yd = rd - df[ri]
+    yd = df[ri] - rd
+    if i > ax.size:
+      break
+    ax.flat[i].bar(np.arange(len(rd)), yd, tick_label = rd.index)
+    ax.flat[i].set_title(ri)
+    i += 1
+
+  plt.show()
+
 def pd_diff_bar_3d(df):
   z = 0
   fig, ax = plt.subplots(subplot_kw=dict(projection='3d'))
-  plt.set_cmap('Paired')
+  plt.set_cmap('Spectral')
   cmap = plt.get_cmap()
   for ri,rd in df.iterrows():
-    yd = rd - df[ri]
-    ax.bar(np.arange(len(rd)), yd, zs=z, zdir='y', color=cmap(z / len(df)))
+    #yd = rd - df[ri]
+    yd = df[ri] - rd
+    ax.bar(np.arange(len(rd)), yd, zs=z, zdir='y', color=cmap(z / len(df)), label=ri)
     z += 1
 
   ax.set_xticks(np.arange(df.shape[0]))
   ax.set_xticklabels(df.columns)
   ax.set_yticks(np.arange(df.shape[1]))
   ax.set_yticklabels(df.index)
+  fig.legend()
   plt.show()
 
 def pd_diff_scatter_3d(xc, yc, zc, s1, s2):
@@ -59,85 +82,127 @@ def pd_diff_scatter_3d(xc, yc, zc, s1, s2):
     plt.figtext(0.4, 0.5, "✔ all data is equal")
   plt.show()
 
-def main(input_a, key_a, group_a, value_a, input_b, key_b, group_b, value_b, condition, output_matrix, output_records = None, chart_bar = None, chart_scatter = None, x = None, y = None, z = None):
-
-  df_a = pd_load_dataframe(input_a, condition, None, [key_a, group_a, value_a])
-  df_b = pd_load_dataframe(input_b, condition, None, [key_b, group_b, value_b])
-  df_a.fillna('null', inplace=True)
-  df_b.fillna('null', inplace=True)
-
-  if key_a:
-    df_a.set_index(key_a.split(','), drop=False, inplace=True)
-  if key_b:
-    df_b.set_index(key_b.split(','), drop=False, inplace=True)
-
-  axb = dict()
-  aks = set()
-  arl = list()
-  g_a = None
-  for i_a,row_a in df_a.iterrows():
-    if group_a:
-      g_a = row_a[group_a]
-    aks.add(g_a)
-    v_a = 1
-    if value_a:
-      v_a = row_a[value_a]
-    g_b, v_b = None, None
-    if i_a in df_b.index:
-      row_b = df_b.loc[i_a]
-      if np.ndim(row_b) == 2:
-        row_b = row_b.max(0)
-      if group_b:
-        g_b = row_b[group_b]
-      for k in [g_a, g_b]:
-        if k not in axb:
-          axb[k] = dict([(g_a, 0), (g_b, 0)])
-        if g_b not in axb[k]:
-          axb[k][g_b] = 0
-
-      aks.add(g_b)
-      v_b = 1
-      if value_b:
-        v_b = row_b[value_b]
-      axb[g_a][g_b] += v_b - max(0, v_b - v_a)
-      axb[g_a][g_a] += max(0, v_a - v_b)
-      axb[g_b][g_b] += max(0, v_b - v_a)
-    arl.append([i_a,g_a,v_a,g_b,v_b])
-
-  aks = sorted(aks)
-
-  name_a = "%s:%s" % (group_a, os.path.basename(input_a))
-  name_b = "%s:%s" % (group_b, os.path.basename(input_b))
-  df_axb = pd.DataFrame(np.zeros((len(aks), len(aks))), pd.MultiIndex.from_product([[name_a], aks]), pd.MultiIndex.from_product([[name_b], aks]))
-  for row_a in df_axb.index:
-    for row_b in df_axb.columns:
-      df_axb.loc[row_a, row_b] = axb.get(row_a[1], {}).get(row_b[1], 0)
-
-  if output_matrix:
-    if sys.hexversion < 0x3060000:
-      df_axb.to_excel(output_matrix)
-    else:
-      df_axb.style.apply(fn_eye_style, None).to_excel(output_matrix)
+def pd_major_sum(_):
+  r = np.nan
+  if _.dtype.num == 17:
+    vc = _.value_counts()
+    if vc.size:
+      r = vc.idxmax()
   else:
-    print(df_axb.to_string())
-  if output_records:
-    df = pd.DataFrame(arl, columns=[key_a, name_a, value_a or 'a', name_b, value_b or 'b'])
-    pd_save_dataframe(df, output_records)
-  if int(chart_bar):
-    df = None
-    if sys.hexversion < 0x3060000:
-      df = df_axb.copy()
-      df.index = df.index.levels[1]
-      df.columns = df.columns.levels[1]
+    r = np.nansum(_)
+  return r
+
+def pd_ijk_merge_delta(il, condition):
+  dfv = []
+  dfg = []
+  dfa = None
+  for i in range(len(il)):
+    fn,groups,key,value = il[i]
+    gl = groups.split(';')
+    bn = os.path.splitext(os.path.basename(fn))[0]
+    vi = '%s:%s:%d' % (bn, value, i)
+    dfv.append(vi)
+    gs = ['%s:%s:%d' % (bn,_,i) for _ in gl]
+    dfg.append(gs)
+    kl = None
+    if key:
+      kl = key.split(';')
+    df = pd_load_dataframe(fn, condition, None, [value] + kl + gl)
+    if not value:
+      value = vi
+      df[value] = 1
+    df.rename(columns=dict(((value, vi),) + tuple(zip(gl, gs))), inplace=True)
+    if kl is not None:
+      df = df.pivot_table([vi] + gs, kl, None, pd_major_sum)
+
+    if sys.hexversion < 0x3080000:
+      if df.ndim == 1:
+        # pd.merge in older versions only works on dataframes
+        df = df.to_frame()
+      df.reset_index(inplace=True)
+      #df.to_excel('merge_%d.xlsx' % i)
+      if dfa is None:
+        dfa = df
+      else:
+        dfa = pd.merge(dfa, df, 'outer', kl)
     else:
-      df = df_axb.droplevel(0).droplevel(0, 1)
-    pd_diff_bar_3d(df)
+      if dfa is None:
+        dfa = df
+      else:
+        dfa = pd.merge(dfa, df, 'outer', left_index=True, right_index=True)
 
-  if int(chart_scatter):
-    pd_diff_scatter_3d(df_a[x], df_a[y], df_a[z], [_[1] for _ in arl], [_[3] for _ in arl])
+  if sys.hexversion >= 0x3080000:
+    dfa.reset_index(inplace=True)
+  # remove nans lest they crash sort_values
+  for gl in dfg:
+    for g in gl:
+      dfa[g].fillna('', inplace=True)
 
-  print("finished")
+  return dfa, dfg, dfv
 
+def pd_delta(dfa, dfg, dfv):
+  log('index')
+  aks = None
+  for d in dfg:
+    r = pd.Index(dfa[d].drop_duplicates())
+    if aks is None:
+      aks = r
+    else:
+      aks = aks.union(r)
+
+  aks = aks.sort_values()
+  
+  log('repivot')
+  df = pd.DataFrame(np.zeros((len(aks), len(aks))), aks.copy(','.join(dfg[0])), aks.copy(','.join(dfg[1])))
+
+  for ri, rd in dfa.iterrows():
+    if ri % 10000 == 0:
+      log(ri,'/',dfa.shape[0],'records processed')
+    g0 = tuple(rd[dfg[0]])
+    g1 = tuple(rd[dfg[1]])
+    v0 = rd[dfv[0]]
+    v1 = rd[dfv[1]]
+    #   df.loc[g0, g1] += min(v0, v1)
+    #   df.loc[g0, g0] += max(0, v0 - v1)
+    #   df.loc[g1, g1] += max(0, v1 - v0)
+    # .loc not working for some reason
+    df[g1][g0] += min(v0, v1)
+    df[g0][g0] += max(0, v0 - v1)
+    df[g1][g1] += max(0, v1 - v0)
+
+  if ri < dfa.shape[0]:
+    log(dfa.shape[0],'/',dfa.shape[0],'records processed')
+
+  return df
+
+
+def db_delta(input_a, keys_a, groups_a, value_a, input_b, keys_b, groups_b, value_b, condition, output_matrix, percent = False, output_records = None):
+  il = [[input_a, groups_a, keys_a, value_a],[input_b, groups_b, keys_b, value_b]]
+
+  log('merge')
+  dfa, dfg, dfv = pd_ijk_merge_delta(il, condition)
+  df = pd_delta(dfa, dfg, dfv)
+
+  if int(percent):
+    df /= df.sum()
+
+  #df.columns = pd.MultiIndex.from_tuples([('', ' '.join(_)) for _ in df.columns], name=[df.columns.name,''])
+  df.index.values[:] = np.array(df.index.map(lambda _: ' '.join(_)))
+
+  log('output matrix')
+  if output_matrix:
+    df.to_excel(output_matrix, index=True)
+    #df.style.apply(fn_eye_style, None).to_excel(output_matrix)
+  else:
+    print(df.to_string())
+
+  if output_records:
+    log('output records')
+    dfa.to_csv(output_records, index=False)
+
+  log("finished")
+
+main = db_delta
 
 if __name__=="__main__":
   usage_gui(__doc__)
